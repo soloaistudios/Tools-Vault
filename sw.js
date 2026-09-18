63577cd8b9472213bc00513354e71f91dc0148a3
soloaistudios <xavionstudios@gmail.com>
2026-09-18 08:17:31 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = 'toolvault-v1';
-const APP_SHELL = ['index.html','tools.html','categories.html','tool.html','blog.html','about.html','contact.html','style.css','responsive.css','app.js','tools.js','categories.js','search.js','filters.js','tool-details.js','blog.js','affiliate.js','tools.json','categories.json','blog.json','manifest.json','icon-192.svg','icon-512.svg'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())));
-self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { const copy=response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request,copy)); return response; }).catch(() => caches.match('index.html')))); });
